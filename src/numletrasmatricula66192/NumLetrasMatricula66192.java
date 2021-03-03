17aba6472ada1dd716941e7455fc6933cec4bee8
acs <acs@win10>
2021-03-02 18:50:56 -0600

    primerCommitNumLetras

---
+++ src/numletrasmatricula66192/NumLetrasMatricula66192.java
@@ -0,0 +1,341 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numletrasmatricula66192;
+import java.util.Scanner;
+/**
+ *
+ * @author acs
+ */
+public class NumLetrasMatricula66192 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    Scanner nums = new Scanner(System.in);
+        
+    int numero = 0;
+        
+    System.out.println("Número a convertir: ");
+    numero = nums.nextInt();
+        
+        
+    if (numero >=0 && numero <=99 ) {
+        
+        switch (numero){
+            case 0:
+            System.out.println("Convertido a letras es: Cero");
+            break;   
+            case 1:
+            System.out.println("Convertido a letras es: Uno");
+            break;
+            case 2:
+            System.out.println("Convertido a letras es: Dos");
+            break;
+            case 3:
+            System.out.println("Convertido a letras es: Tres");
+            break;
+            case 4:
+            System.out.println("Convertido a letras es: Cuatro");
+            break;
+            case 5:
+            System.out.println("Convertido a letras es: Cinco");
+            break;
+            case 6:
+            System.out.println("Convertido a letras es: Seis");
+            break;
+            case 7:
+            System.out.println("Convertido a letras es: Siete");
+            break;   
+            case 8:
+            System.out.println("Convertido a letras es: Ocho");
+            break;
+            case 9:
+            System.out.println("Convertido a letras es: Nueve");
+            break;
+            case 10:
+            System.out.println("Convertido a letras es: Diez");
+            break;
+            case 11:
+            System.out.println("Convertido a letras es: Once");
+            break;
+            case 12:
+            System.out.println("Convertido a letras es: Doce");
+            break;
+            case 13:
+            System.out.println("Convertido a letras es: Trece");
+            break;
+            case 14:
+            System.out.println("Convertido a letras es: Catorce");
+            break;   
+            case 15:
+            System.out.println("Convertido a letras es: Quince");
+            break;
+            case 16:
+            System.out.println("Convertido a letras es: Dieciseis");
+            break;
+            case 17:
+            System.out.println("Convertido a letras es: Diecisiete");
+            break;
+            case 18:
+            System.out.println("Convertido a letras es: Dieciocho");
+            break;
+            case 19:
+            System.out.println("Convertido a letras es: Diecinueve");
+            break;
+            case 20:
+            System.out.println("Convertido a letras es: Veinte");
+            break;
+            case 21:
+            System.out.println("Convertido a letras es: Veintiuno");
+            break;   
+            case 22:
+            System.out.println("Convertido a letras es: Veintidos");
+            break;
+            case 23:
+            System.out.println("Convertido a letras es: Veintitres");
+            break;
+            case 24:
+            System.out.println("Convertido a letras es: Veinticuatro");
+            break;
+            case 25:
+            System.out.println("Convertido a letras es: Veinticinco");
+            break;
+            case 26:
+            System.out.println("Convertido a letras es: Veintiseis");
+            break;
+            case 27:
+            System.out.println("Convertido a letras es: Veintisiete");
+            break;
+            case 28:
+            System.out.println("Convertido a letras es: Veintiocho");
+            break;   
+            case 29:
+            System.out.println("Convertido a letras es: Veintinueve");
+            break;
+            case 30:
+            System.out.println("Convertido a letras es: Treinta");
+            break;
+            case 31:
+            System.out.println("Convertido a letras es: Treinta y uno");
+            break;
+            case 32:
+            System.out.println("Convertido a letras es: Treinta y dos");
+            break;
+            case 33:
+            System.out.println("Convertido a letras es: Treinta y tres");
+            break;
+            case 34:
+            System.out.println("Convertido a letras es: Treinta y cuatro");
+            break;
+            case 35:
+            System.out.println("Convertido a letras es: Treinta y cinco");
+            break;   
+            case 36:
+            System.out.println("Convertido a letras es: Treinta y seis");
+            break;
+            case 37:
+            System.out.println("Convertido a letras es: Treinta y siete");
+            break;
+            case 38:
+            System.out.println("Convertido a letras es: Treinta y ocho");
+            break;
+            case 39:
+            System.out.println("Convertido a letras es: Treinta y nueve");
+            break;
+            case 40:
+            System.out.println("Convertido a letras es: Cuarenta");
+            break;
+            case 41:
+            System.out.println("Convertido a letras es: Cuarenta y uno");
+            break;
+            case 42:
+            System.out.println("Convertido a letras es: Cuarenta y dos");
+            break;   
+            case 43:
+            System.out.println("Convertido a letras es: Cuarenta y tres");
+            break;
+            case 44:
+            System.out.println("Convertido a letras es: Cuarenta y cuatro");
+            break;
+            case 45:
+            System.out.println("Convertido a letras es: Cuarenta y cinco");
+            break;
+            case 46:
+            System.out.println("Convertido a letras es: Cuarenta y seis");
+            break;
+            case 47:
+            System.out.println("Convertido a letras es: Cuarenta y siete");
+            break;
+            case 48:
+            System.out.println("Convertido a letras es: Cuarenta y ocho");
+            break;
+            case 49:
+            System.out.println("Convertido a letras es: Cuarenta y nueve");
+            break;   
+            case 50:
+            System.out.println("Convertido a letras es: Cincuenta");
+            break;
+            case 51:
+            System.out.println("Convertido a letras es: Cincuenta y uno");
+            break;
+            case 52:
+            System.out.println("Convertido a letras es: Cincuenta y dos");
+            break;
+            case 53:
+            System.out.println("Convertido a letras es: Cincuenta y tres");
+            break;
+            case 54:
+            System.out.println("Convertido a letras es: Cincuenta y cuatro");
+            break;
+            case 55:
+            System.out.println("Convertido a letras es: Cincuenta y cinco");
+            break;
+            case 56:
+            System.out.println("Convertido a letras es: Cincuenta y seis");
+            break;   
+            case 57:
+            System.out.println("Convertido a letras es: Cincuenta y siete");
+            break;
+            case 58:
+            System.out.println("Convertido a letras es: Cincuenta y ocho");
+            break;
+            case 59:
+            System.out.println("Convertido a letras es: Cincuenta y nueve");
+            break;
+            case 60:
+            System.out.println("Convertido a letras es: Sesenta");
+            break;
+            case 61:
+            System.out.println("Convertido a letras es: Sesenta y uno");
+            break;
+            case 62:
+            System.out.println("Convertido a letras es: Sesenta y dos");
+            break;
+            case 63:
+            System.out.println("Convertido a letras es: Sesenta y tres");
+            break;   
+            case 64:
+            System.out.println("Convertido a letras es: Sesenta y cuatro");
+            break;
+            case 65:
+            System.out.println("Convertido a letras es: Sesenta y cinco");
+            break;
+            case 66:
+            System.out.println("Convertido a letras es: Sesenta y seis");
+            break;
+            case 67:
+            System.out.println("Convertido a letras es: Sesenta y siete");
+            break;
+            case 68:
+            System.out.println("Convertido a letras es: Sesenta y ocho");
+            break;
+            case 69:
+            System.out.println("Convertido a letras es: Sesenta y nueve");
+            break;
+            case 70:
+            System.out.println("Convertido a letras es: Setenta");
+            break;   
+            case 71:
+            System.out.println("Convertido a letras es: Setenta y uno");
+            break;
+            case 72:
+            System.out.println("Convertido a letras es: Setenta y dos");
+            break;
+            case 73:
+            System.out.println("Convertido a letras es: Setenta y tres");
+            break;
+            case 74:
+            System.out.println("Convertido a letras es: Setenta y cuatro");
+            break;
+            case 75:
+            System.out.println("Convertido a letras es: Setenta y cinco");
+            break;
+            case 76:
+            System.out.println("Convertido a letras es: Setenta y seis");
+            break;
+            case 77:
+            System.out.println("Convertido a letras es: Setenta y siete");
+            break;   
+            case 78:
+            System.out.println("Convertido a letras es: Setenta y ocho");
+            break;
+            case 79:
+            System.out.println("Convertido a letras es: Setenta y nueve");
+            break;
+            case 80:
+            System.out.println("Convertido a letras es: Ochenta");
+            break;
+            case 81:
+            System.out.println("Convertido a letras es: Ochenta y uno");
+            break;
+            case 82:
+            System.out.println("Convertido a letras es: Ochenta y dos");
+            break;
+            case 83:
+            System.out.println("Convertido a letras es: Ochenta y tres");
+            break;
+            case 84:
+            System.out.println("Convertido a letras es: Ochenta y cuatro");
+            break;   
+            case 85:
+            System.out.println("Convertido a letras es: Ochenta y cinco");
+            break;
+            case 86:
+            System.out.println("Convertido a letras es: Ochenta y seis");
+            break;
+            case 87:
+            System.out.println("Convertido a letras es: Ochenta y siete");
+            break;
+            case 88:
+            System.out.println("Convertido a letras es: Ochenta y ocho");
+            break;
+            case 89:
+            System.out.println("Convertido a letras es: Ochenta y nueve");
+            break;
+            case 90:
+            System.out.println("Convertido a letras es: Noventa");
+            break;
+            case 91:
+            System.out.println("Convertido a letras es: Noventa y uno");
+            break;   
+            case 92:
+            System.out.println("Convertido a letras es: Noventa y dos");
+            break;
+            case 93:
+            System.out.println("Convertido a letras es: Noventa y tres");
+            break;
+            case 94:
+            System.out.println("Convertido a letras es: Noventa y cuatro");
+            break;
+            case 95:
+            System.out.println("Convertido a letras es: Noventa y cinco");
+            break;
+            case 96:
+            System.out.println("Convertido a letras es: Noventa y seis");
+            break;
+            case 97:
+            System.out.println("Convertido a letras es: Noventa y siete");
+            break;
+            case 98:
+            System.out.println("Convertido a letras es: Noventa y ocho");
+            break;   
+            case 99:
+            System.out.println("Convertido a letras es: Noventa y nueve");
+            break;
+            
+        }
+        
+        
+    } else{
+            
+            System.out.println("Favor de ingresar un número con rango entre 0 y 99 ");
+          
+    
+}
+}
+}
